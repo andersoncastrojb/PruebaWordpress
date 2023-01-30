af4421cfbd023a19f83dbba9c50ac95d19716498
mauneven <mauneven@gmail.com>
2023-01-30 17:10:01 -0500

    navbar update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3518dfd62a0f6c53"; }
+/******/ 		__webpack_require__.h = function() { return "8eec552afc677056"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Navigation.js
+++ components/Navigation.js
@@ -1,39 +1,79 @@
 import Link from "next/link";
 
 const Navigation = () => (
-<nav className ="navbar navbar-expand-lg bg-body-tertiary">
-  <div className ="container-fluid">
-    <a className ="navbar-brand" href="#">Navbar scroll</a>
-    <button className ="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-      <span className ="navbar-toggler-icon"></span>
+<nav class="navbar navbar-expand-lg bg-body-tertiary border">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <img src="https://www.bebolder.co/wp-content/uploads/2022/03/favicon-Bebolder.png" alt="mdo" width="50" height="auto" class=""/>
+</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar">Menu</span>
     </button>
-    <div className ="collapse navbar-collapse" id="navbarScroll">
-      <ul className ="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
-        <li className ="nav-item">
-          <a className ="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li className ="nav-item">
-          <a className ="nav-link" href="#">Link</a>
-        </li>
-        <li className ="nav-item dropdown">
-          <a className ="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Link
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Bebolder
           </a>
-          <ul className ="dropdown-menu">
-            <li><a className ="dropdown-item" href="#">Action</a></li>
-            <li><a className ="dropdown-item" href="#">Another action</a></li>
-            <li><hr className ="dropdown-divider" /></li>
-            <li><a className ="dropdown-item" href="#">Something else here</a></li>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
           </ul>
         </li>
-        <li className ="nav-item">
-          <a className ="nav-link disabled">Link</a>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Departments
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Training
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Blog
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Workspace
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li  class="nav-item dropdown bi bi-bell-fill">
+        <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle pt-1" data-bs-toggle="dropdown" aria-expanded="false">
+            <img src="https://www.pngall.com/wp-content/uploads/11/Youtube-Bell-Icon-Notification-Yellow-Button-PNG-Clipart.png" alt="mdo" width="32" height="32" class="rounded-circle"/>
+          </a>
+          <ul class="dropdown-menu text-small" >
+            <li><a class="dropdown-item" href="#">Notification 1</a></li>
+            <li><a class="dropdown-item" href="#">Notification 2</a></li>
+            <li><a class="dropdown-item" href="#">Notification 3</a></li>
+            <li><hr/></li>
+            <li><a class="dropdown-item" href="#">Sign out</a></li>
+          </ul>
         </li>
       </ul>
-      <form className ="d-flex" role="search">
-        <input className ="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className ="btn btn-outline-success" type="submit">Search</button>
-      </form>
     </div>
   </div>
 </nav>
